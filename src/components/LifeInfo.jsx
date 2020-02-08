bbf9c2ec6b3e6bacd5d9d3c9a1b0091d0b3d6bfe
Natalalia <n.olmo.megias@gmail.com>
2020-02-08 17:40:19 +0000

    getting updated time every 5 seconds

--- src/components/LifeInfo.jsx
+++ src/components/LifeInfo.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 
 class LifeInfo extends Component {
-  state = {};
+  state = {
+    time: null
+  };
+
+  componentDidMount() {
+    this.setState({
+      time: Date.now()
+    });
+    this.interval = setInterval(() => {
+      this.setState({
+        time: Date.now()
+      });
+    }, 5000); // quizá luego debería ser el tiempo cada segundo
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return <h2>Life info</h2>;
+    return (
+      <>
+        <span>{new Date().toLocaleTimeString("en-US", { hour12: false })}</span>
+        <h2>Temperature:</h2>
+        <span>temperature here</span>
+        <h2>Power:</h2>
+        <span>power here</span>
+      </>
+    );
   }
 }
 
